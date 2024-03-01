0ad9145317b63cbb82dba788b5bdc4b1956376fe
Artur Alves <arturalves.ca@gmail.com>
2024-03-01 19:20:09 +0100

    viz embeddings function

--- src/graphlp/visualize.py
+++ src/graphlp/visualize.py
@@ -1,12 +1,28 @@
 
 from typing import Callable, List
 import numpy as np
-
+import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
 
 def visualize_embeddings(
     embedding_matrix: np.ndarray,
     words: List[str],
     get_word_index: Callable[[str], int]
 ) -> None:
-    ...
 
+    # embedding dim should be 2D or 3D
+    embedding_dim = embedding_matrix.shape[1]
+    assert 2 <= embedding_dim <= 3
+   
+    fig = plt.figure()
+
+    if embedding_dim == 3:
+        ax = fig.add_subplot(111, projection='3d')
+    else: # dim == 2 
+        ax = fig.add_subplot(111)
+
+    for word in enumerate(words):
+        emb_axis = [embedding_matrix[get_word_index(word), k] for k in range(len(embedding_dim))]
+        ax.text(*emb_axis, word, zorder=1)
+
+    plt.show()
