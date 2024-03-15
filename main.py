a8883020ffcd12d419a37cee963452af9ef39ab1
Guile Vieira <guilhermevmanahes@gmail.com>
2024-03-15 20:05:16 +0100

    other similarities

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ import nltk
 
 
 from graphlp.graph_of_words import GraphOfWords
-from graphlp.similarity import path_word_similarity
-from graphlp import embedding_model
+from graphlp import embedding_model, similarity as sim
 from graphlp.visualize import visualize_embeddings
 
 
@@ -20,7 +19,8 @@ class Configuration:
     radius: int = 3
     corpus: Union[Literal["brown"], Literal["wordnet"]] = "brown"
     training_size: int = 100
-    similarity: Union[Literal["path_word"], None] = "path_word"
+    similarity: Union[Literal["path"],
+                      Literal["lch"], Literal["wup"], None] = "path"
     model: Union[Literal["NLP"], Literal["DGP"], Literal["ISO"]] = "NLP"
     sample_size: int = 3
     # model parameters
@@ -88,8 +88,15 @@ def main():
     graph = GraphOfWords(corpus, radius=config.radius)
 
     print("Enriching the graph-of-words.")
-    if config.similarity == "path_word":
-        graph.enrich(path_word_similarity)
+    if config.similarity == "path":
+        graph.enrich(sim.path_word_similarity)
+    elif config.similarity == "wup":
+        graph.enrich(sim.wup_word_similarity)
+    elif config.similarity == "lch":
+        graph.enrich(sim.lch_word_similarity)
+    else:
+        print("Similaity measure not supported.")
+        return
     adjacency_matrix = graph.adjacency_matrix()
 
     print("Running the model.")
--- src/graphlp/similarity.py
+++ src/graphlp/similarity.py
@@ -1,30 +1,43 @@
+__all__ = ["path_word_similarity",
+           "wup_word_similarity", "lch_word_similarity"]
 from nltk.corpus import wordnet as wn
 
 
-def path_word_similarity(word1, word2):
-    """
-    Calculate similarity between two words using NLTK and WordNet.
-
-    Parameters:
-    word1 (str): The first word.
-    word2 (str): The second word.
-
-    Returns:
-    similarity (float): The similarity score between the two words (0 to 1).
-    """
+def _nltk_word_similarity(
+        word1,
+        word2,
+        comparator
+):
     synsets1 = wn.synsets(word1)
     synsets2 = wn.synsets(word2)
     if not synsets1 or not synsets2:
         return 0.0  # If no synsets are found for either word
 
     max_similarity = 0.0
-
     for synset1 in synsets1:
         for synset2 in synsets2:
             if synset1 is None or synset2 is None:
                 continue
-            similarity = synset1.path_similarity(synset2)
+            similarity = comparator(synset1, synset2)
             if similarity and similarity > max_similarity:
                 max_similarity = similarity
 
     return max_similarity
+
+
+def path_word_similarity(word1, word2):
+    return _nltk_word_similarity(
+        word1, word2,
+        lambda x, y: x.path_similarity(y))
+
+
+def wup_word_similarity(word1, word2):
+    return _nltk_word_similarity(
+        word1, word2,
+        lambda x, y: x.wup_similarity(y))
+
+
+def lch_word_similarity(word1, word2):
+    return _nltk_word_similarity(
+        word1, word2,
+        lambda x, y: x.lch_similarity(y))
